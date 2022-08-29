0dffe3e9c1eccea9616d0249a8e15b1ce5390bcd
Ezhevikaa <v.estro911@gmail.com>
2022-08-29 19:25:56 +0300

    the first python file

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+# x = 5
+# y = 3
+# print(x+y)
+# print(x-y)
+# print(x*y)
+# print(x/y)
+# print(x//y)
+# print(x**y)
+# print(x%y)
+
+# Calculating of credit final sum
+credit = 7000
+credit_rate = 7
+number_of_years = 9
+final_sum = credit + credit / 100 * credit_rate * number_of_years
+
+print(final_sum)
