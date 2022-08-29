9452b6e1e20b2746a6d75b8747433f252cdae3fb
Ezhevikaa <v.estro911@gmail.com>
2022-08-29 19:29:42 +0300

    the next simple python file

---
+++ Next file.py
@@ -0,0 +1,10 @@
+greeting = "Hello"
+first_name = "Jack"
+last_name = 'White'
+exclamation_sign = "!"
+white_space = " "
+whole_sentence = greeting + white_space + first_name + ' ' +last_name + exclamation_sign
+print(whole_sentence)
+print(greeting + white_space + first_name + " " + last_name + exclamation_sign)
+long_string = 'This is the long string'
+print(long_string)
