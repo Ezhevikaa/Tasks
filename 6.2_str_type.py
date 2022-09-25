9adb6c277065121b7c6ff3dd85852c5c54b5fc7d
Ezhevikaa <v.estro911@gmail.com>
2022-09-25 20:18:38 +0300

    решенные задачи по курсу

---
+++ 6.2_str_type.py
@@ -0,0 +1,23 @@
+
+# Напишите программу, которая выводит текст:
+# "Python is a great language!", said Fred. "I don't ever remember having this much fun before."
+# Используйте конкатенацию строк.
+# str1 = '"Python is a great language!", '
+# str2 = 'said Fred. '
+# str3 = '"I don\'t ever remember having this much fun before."'
+# print(str1 + str2 + str3)
+
+# Напишите программу, которая считывает с клавиатуры две строки – имя и фамилию пользователя и выводит фразу:
+# «Hello [введенное имя] [введенная фамилия]! You just delved into Python».
+# firstname = input()
+# lastname = input()
+# a = '"Hello'
+# b = 'You just delved into Python.'
+# print(a + ' ' + firstname + ' ' + lastname + '!' + ' ' + b)
+
+
+# Напишите программу, которая считывает с клавиатуры название футбольной команды и выводит фразу:
+# «Футбольная команда [введённая строка] имеет длину [длина введённой строки] символов».
+# name = input()
+# len1 = len(name)
+# print('Футбольная команда ' + name + ' имеет длину ' + str(len1) + ' символов')
